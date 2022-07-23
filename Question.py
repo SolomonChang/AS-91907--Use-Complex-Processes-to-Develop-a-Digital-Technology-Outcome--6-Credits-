5eb00f6bb63529e324405c9aa68e4573e32baa43
SolomonChang <78829680+SolomonChang@users.noreply.github.com>
2022-07-23 22:12:30 +1200

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,5 +1,7 @@
+# import random code to use
 import random
 
+# class for the questions
 class Question:
   def __init__(self, description, options):
     self.description = description
@@ -7,19 +9,25 @@ class Question:
     index = random.randint(0, 2)
     self.options[0], self.options[index] = self.options[index], self.options[0]
     self.answer_index = index
-
+    
+# Funtion for the question
   def get_question(self):
     return(self.description)
-  
+ 
+# Funtion for the option  
   def get_option1(self):
     return(self.options[0])
   
+  # Funtion for the option  
   def get_option2(self):
     return(self.options[1])
-    
+  
+    # Funtion for the option  
   def get_option3(self):
     return(self.options[2])
-    
+  
+    # Funtion for the answer 
   def answer_check(self, number):
     return number == self.answer_index
+    
     
