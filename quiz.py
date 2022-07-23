8eba613eec3b56cbff61018011d868ea542c8f49
SolomonChang <78829680+SolomonChang@users.noreply.github.com>
2022-07-23 22:09:36 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,11 +1,15 @@
+
+# importing different code into the program for it to work.
 from tkinter import *
 from tkinter import font
+#importing an other class group for the program to use.
 from Question import *
 import random
 
 
 Desired_font = ("Comic Sans MS", 10, "bold")
 
+#Function for the code when the start again button is clicked. It restarts the program.
 def start_again():
     global Score,Question_no
     
@@ -23,6 +27,7 @@ def start_again():
     score_output.place_forget()
     root.pack()
     
+#Function for the next button to submit the chossen answer.    
 def next():
     global Score,Question_no
     if(val1.get()):
@@ -47,7 +52,7 @@ def next():
         root.pack_forget()
         score_output.place(relx=.45,rely=.30)
         play_again.place(relx = .45,rely = .60)
-        score_output.config(text = "Score: " +str(Score))
+        score_output.config(text = "Total: " +str(Score))
 
     else:
         val1.set(0) 
@@ -58,6 +63,7 @@ def next():
         option2.config(text=Questions[Question_no-1].get_option2())
         option3.config(text=Questions[Question_no-1].get_option3())
 
+#function to check the answer if it is correct or not
 def Check(Option):
     if(Option == 1):
         # val1.set(1)
@@ -74,7 +80,8 @@ def Check(Option):
         val1.set(0)
         val2.set(0)
         # val3.set(1)
-        
+    
+#variables for the randomly genetrated questions    
 num1 = random.randint(1, 9)
 num2 = random.randint(1, 9)
 num3 = random.randint(1, 9)
@@ -82,6 +89,7 @@ num4 = random.randint(1, 9)
 num5 = random.randint(1, 9)
 num6 = random.randint(1, 9)
 
+# The 3 questions for the user to answer.
 q1= Question("What is " + str(num1) + "+" + str(num2) +"?" , [num1 + num2, num1 * num2,num1 -num2])
 q2= Question("What is " + str(num3) + "-" + str(num4) +"?" , [num3 -num4, num3 * num4, num3 + num4])
 q3= Question("What is " + str(num5) + "*" + str(num6) +"?" , [num5 * num6, num5 + num6,num5 -num6])
@@ -89,15 +97,18 @@ Questions = [q1, q2, q3]
 
         
 
-
+# Variables  used to set the score and total
 Score = 0
 Total_No_Questions = 3
 Question_no = 1
 
 Win= Tk()
+#the size of the window of the program
 Win.geometry("500x300")
+# The title of the program
 Win.title("Basic Math quiz")
 
+# The title description 
 label = Label(
     text="Welcome to a Basic Math quiz"
 )
@@ -118,6 +129,7 @@ question = Label(root,width = 60,font=(10),text=Questions[0].get_question())
 question.configure(font = Desired_font)
 question.pack(fill=X)
 
+# 3 variables 
 val1 = IntVar()
 val2 = IntVar()
 val3 = IntVar()
@@ -131,18 +143,19 @@ option2.pack()
 option3 = Checkbutton(root,variable=val3,text=Questions[Question_no-1].get_option3(),command=lambda:Check(3))
 option3.pack()
 
+# gui of the button for the submit
 next_b = Button(root , text = "Submit",command=next)
 next_b.configure(font = Desired_font)
 next_b.pack()
 
 
-
+# The Gui for total
 score_output = Label(Win,font=(50))
 score_output.configure(font = Desired_font)
 score_output.place_forget()
 
 
-
+# gui button for the play again button
 play_again = Button(Win,text= "Play Again",command=start_again)
 play_again.configure(font = Desired_font)
 play_again.place_forget()
